93b82332a4673fdc75caf9d8cc9f8e79aa17ade0
Cezar Halmagean <cezar@mixandgo.com>
2016-07-04 15:58:44 +0300

    Move default formatter to a separate class

---
+++ lib/prime_tables/default_formatter.rb
@@ -0,0 +1,8 @@
+module PrimeTables
+  class DefaultFormatter
+    def render(array)
+      array
+    end
+  end
+end
+
--- lib/prime_tables/multiplication.rb
+++ lib/prime_tables/multiplication.rb
@@ -7,13 +7,17 @@ module PrimeTables
       raise ArgumentError, 'Size should be greater than 1' if @size < 1
     end
 
-    def render
-      primes.map do |y|
-        primes.map { |x|  y * x }.unshift(y)
-      end.unshift([0, *primes])
+    def render(renderer = DefaultFormatter.new)
+      renderer.render(multiplication_table)
     end
 
     private
+
+      def multiplication_table
+        primes.map do |y|
+          primes.map { |x|  y * x }.unshift(y)
+        end.unshift([0, *primes])
+      end
 
       def primes
         # Cache the result so we only calculate the prime numbers once
