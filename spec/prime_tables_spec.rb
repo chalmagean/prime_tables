2dbf69f047ddb0e9a941a81e2f9c2c2f9b183ecb
Cezar Halmagean <cezar@mixandgo.com>
2016-07-04 15:59:02 +0300

    Adjust initial expectation

--- spec/prime_tables_spec.rb
+++ spec/prime_tables_spec.rb
@@ -7,15 +7,15 @@ describe PrimeTables do
 
   it 'prints a multiplication table' do
     expected = <<-END
-|      |    2 |    3 |    5 |
-|    2 |    4 |    6 |   10 |
-|    3 |    6 |    9 |   15 |
-|    5 |   10 |   15 |   25 |
+|     |   2 |   3 |   5 |
+|   2 |   4 |   6 |  10 |
+|   3 |   6 |   9 |  15 |
+|   5 |  10 |  15 |  25 |
 END
 
     table = PrimeTables::Multiplication.new(3)
     formatter = PrimeTables::TextFormatter.new
 
-    expect(table.render(formatter)).to output(expected)
+    expect { table.render(formatter) }.to output(expected).to_stdout
   end
 end
